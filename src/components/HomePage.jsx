c6d5a6386487c36eba5c55ef9df6628a1539ee23
Yashu Bansal <143242810+yashubansal000@users.noreply.github.com>
2025-05-16 14:49:20 +0530

    HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,1 +1,15 @@
-const HomePage = () => <div>Welcome to Algorithm Explorer</div>; export default HomePage;
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Welcome to Algorithm Explorer</h1>
+      <p>Choose an algorithm to explore:</p>
+      <Link to="/huffman">Visualize Huffman</Link><br />
+      <Link to="/naive">Visualize Naive Search</Link>
+    </div>
+  );
+}
+
+export default HomePage;
